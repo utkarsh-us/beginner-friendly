f19e45172c7e229f6f98963fd7022b74f2818164
Daniel Tayal <tayaldaniel2@gmail.com>
2024-10-22 15:01:17 +0530

    Update palindrome.py

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,9 +1,28 @@
-"""I am using string slicing method to do this check, i know one more method that is using recursion feel free to edit and contribute <3"""
+"""I am using string slicing method to do this check, i know one more method that is using recursion that i have commented down feel free to edit and contribute <3"""
 
 # main code
 def is_palindrome(n):
-    return n == n[::-1]
+    return n == n[::-1] # if n == reversed n
 
-# n = "racecar"  -->   example
-# print(is_palindrome(n)) 
+n = input() # user will give input
+print(is_palindrome(n))  # calling the function and then printing boolean - True or False
+
+
+
+"""
+RECURSION METHOD!
+
+def is_palindrome(n):
+    if len(n) <= 1: # number of characters in n
+        return True
+    if n[0] != n[-1]: # if first character not equal to last character
+        return False
+    return is_palindrome(s[1:-1]) # if second character == second last character
+
+# usage
+
+n = input()
+print(is_palindrome(n))
+
+"""
 
