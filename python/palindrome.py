9f88cdb4ae896a56122cef19abcc04e04d3aafc1
Daniel Tayal <tayaldaniel2@gmail.com>
2024-10-22 14:50:14 +0530

    Update palindrome.py

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,1 +1,9 @@
+"""I am using string slicing method to do this check, i know one more method that is using recursion feel free to edit and contribute <3"""
+
+# main code
+def is_palindrome(n):
+    return n == n[::-1]
+
+# n = "racecar"  -->   example
+# print(is_palindrome(n)) 
 
